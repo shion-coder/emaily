6e241d11820e9210824e6b6ac8eae5e3fe857576
Shion <shionchill@Shions-MacBook-Pro.local>
2020-05-02 16:07:48 +0700

    Deploy

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const passpost = require("passport");
-const cookieSession = require("cookie-session");
+// const passpost = require("passport");
+// const cookieSession = require("cookie-session");
 
-const { cookieKey } = require("./config/keys");
+// const { cookieKey } = require("./config/keys");
 
 /* -------------------------------------------------------------------------- */
 
@@ -11,14 +11,18 @@ const app = express();
 // Midllewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [cookieKey],
-  })
-);
-app.use(passpost.initialize());
-app.use(passpost.session());
+// app.use(
+//   cookieSession({
+//     maxAge: 30 * 24 * 60 * 60 * 1000,
+//     keys: [cookieKey],
+//   })
+// );
+// app.use(passpost.initialize());
+// app.use(passpost.session());
+
+app.get("/", (req, res) => {
+  res.send("Hellooooo");
+});
 
 /* -------------------------------------------------------------------------- */
 
