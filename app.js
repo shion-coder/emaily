9b7793b7d8431421a58a65874f4f52317d0d8513
Shion <shionchill@Shions-MacBook-Pro.local>
2020-05-02 16:02:42 +0700

    Deploy

--- app.js
+++ app.js
@@ -9,20 +9,16 @@ const { cookieKey } = require("./config/keys");
 const app = express();
 
 // Midllewares
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(
-//   cookieSession({
-//     maxAge: 30 * 24 * 60 * 60 * 1000,
-//     keys: [cookieKey],
-//   })
-// );
-// app.use(passpost.initialize());
-// app.use(passpost.session());
-
-app.get("/", (req, res) => {
-  res.send("Heloooo");
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [cookieKey],
+  })
+);
+app.use(passpost.initialize());
+app.use(passpost.session());
 
 /* -------------------------------------------------------------------------- */
 
--- index.js
+++ index.js
@@ -1,37 +1,31 @@
-// const mongoose = require("mongoose");
-// const path = require("path");
-// const express = require("express");
-// const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
 
 const app = require("./app");
-// const { mongoURI } = require("./config/keys");
+const { mongoURI } = require("./config/keys");
 
-// require("./models/user");
-// require("./services/passpost");
+require("./models/user");
+require("./services/passpost");
 
 /* -------------------------------------------------------------------------- */
 
-// // Connect Mongo Database
-// mongoose.connect(mongoURI, {
-//   useNewUrlParser: true,
-//   useCreateIndex: true,
-//   useUnifiedTopology: true,
-// });
+// Connect Mongo Database
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
-// // Routes
-// require("./routes/auth-routes")(app);
-// require("./routes/billing-routes")(app);
+// Routes
+require("./routes/auth-routes")(app);
+require("./routes/billing-routes")(app);
 
-// app.get("/", (req, res) => {
-//   res.send("Heloooo");
-// });
-
-// // Serve when production
+// Serve when production
 // if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "client/build")));
+//   app.use(express.static("client/build"));
 
 //   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "client/build", "index.html"));
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 //   });
 // }
 
