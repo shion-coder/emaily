e4e4195d8a1cf9b7355692d18d855457687d2682
Shion <shionchill@Shions-MacBook-Pro.local>
2020-05-02 16:06:00 +0700

    Deploy

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const mongoose = require("mongoose");
-const path = require("path");
+// const mongoose = require("mongoose");
+// const path = require("path");
 
 const app = require("./app");
-const { mongoURI } = require("./config/keys");
+// const { mongoURI } = require("./config/keys");
 
 require("./models/user");
 require("./services/passpost");
 
-/* -------------------------------------------------------------------------- */
-
-// Connect Mongo Database
-mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-});
-
 // Routes
 require("./routes/auth-routes")(app);
 require("./routes/billing-routes")(app);
+
+/* -------------------------------------------------------------------------- */
+
+// Connect Mongo Database
+// mongoose.connect(mongoURI, {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useUnifiedTopology: true,
+// });
 
 // Serve when production
 // if (process.env.NODE_ENV === "production") {
