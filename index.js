4e76fb4c9a4275df993d5a0307b6ea0d96c300a3
Shion <shionchill@Shions-MacBook-Pro.local>
2020-05-02 15:32:21 +0700

    Deploy

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
-// Import
+// const mongoose = require("mongoose");
+// const path = require("path");
+// const express = require("express");
+// const app = express();
+
 const app = require("./app");
+// const { mongoURI } = require("./config/keys");
 
-// ----------------------------------------------------------------------------------------- //
+// require("./models/user");
+// require("./services/passpost");
 
-// Setup
-const port = process.env.PORT || 4000;
+/* -------------------------------------------------------------------------- */
 
-// Serving
-app.listen(port, (error) => {
-  if (error) throw error;
+// // Connect Mongo Database
+// mongoose.connect(mongoURI, {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useUnifiedTopology: true,
+// });
 
-  console.log(`Listening on port: ${port}`);
+// // Routes
+// require("./routes/auth-routes")(app);
+// require("./routes/billing-routes")(app);
+
+app.get("/", (req, res) => {
+  res.send("Heloooo");
 });
+
+// Serve when production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT);
