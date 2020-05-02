9fe1b0db357b37c708f49b630cfeffb1e8d8b63e
Shion <shionchill@Shions-MacBook-Pro.local>
2020-05-02 23:44:36 +0700

    Deploy

--- app.js
+++ app.js
@@ -21,12 +21,12 @@ app.use(
 app.use(passpost.initialize());
 app.use(passpost.session());
 
-// Serve when production
+// Production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 
